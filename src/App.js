30fe0e078681db59d3d0950ffb9933c513425d2c
Ayshafebin <febinayisha0@gmail.com>
2023-12-29 16:07:48 +0530

    UseContext

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ import './App.css';
 // import Counter from './Counter/Counter';
 // import FetchAPI from './Fetch/FetchAPI';
 // import UseReducer from './Reducer/UseReducer';
-// import Context from './Context/Context';
-// import MyContext from './Context/Context';
+import Context from './Context/Context';
+import MyContext from './Context/Context';
 
-import Props from './Component/Props';
+// import Props from './Component/Props';
 // import Counter from './Counter/Counter';
 // import UseReducer from './Reducer/UseReducer';
 
@@ -25,11 +25,11 @@ function App() {
   return (
     <div className="App">
 
-      <Props value={'Props'}/>
+      {/* <Props value={'Props'}/> */}
 
-      {/* <MyContext.Provider value={'Context'}>
+      <MyContext.Provider value={'Context'}>
         <Context/>   
-      </MyContext.Provider> */}
+      </MyContext.Provider>
 
       {/* <Counter/> */}
 
